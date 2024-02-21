b5f01bec1a4fd0f61c619cc32e8d5eea66e1de5f
bigstring-zer0 <147473025+bigstring-zer0@users.noreply.github.com>
2024-02-21 12:05:51 +0900

    github.js 내에 Github Class 생성

---
+++ github.js
@@ -0,0 +1,8 @@
+class Github {
+    constructor() {
+        this.client_id = '1153bc884441c6021ba0';
+        this.client_secret = '32de2869c44253c80028a139363b530cae0e1618';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
+    }
+}
