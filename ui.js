b5d3be0bd3e8afc0d6dede3876ed7ae9c18bc47c
bigstring-zer0 <147473025+bigstring-zer0@users.noreply.github.com>
2024-02-21 12:31:58 +0900

    ui.js 내에 showRepositories 메서드 구현

--- ui.js
+++ ui.js
@@ -32,3 +32,26 @@ function showProfile(user) {
     `;
 }
 
+function showRepositories(repositories) {
+    let text = '';
+
+    repositories.forEach(function (repository) {
+        text += `
+            <div class="card card-body mb-2">
+                <div class="row">
+                    <div class="col-md-6">
+                        <a href="${repository.html_url}" target="_blank">${repository.name}</a>
+                    </div>
+                    <div class="col-md-6">
+                    <span class="badge badge-primary">Starts: ${repository.stargazers_count}</span>
+                    <span class="badge badge-secondary">Watchers: ${repository.watchers_count}</span>
+                    <span class="badge badge-primary">Forks: ${repository.forms_count}</span>
+                    </div>
+                </div>
+            </div>
+        `;
+    });
+
+    document.getElementById('repositories').innerHTML = text;
+}
+
