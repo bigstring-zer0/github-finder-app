2a387f378f62c3cd134c5c63552ae08e7fb29a2c
bigstring-zer0 <147473025+bigstring-zer0@users.noreply.github.com>
2024-02-22 20:10:06 +0900

    간단한 코드 수정

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ searchUser.addEventListener('keyup', (e) => {
         .then(data => {
             if(data.profile.message === 'Not Found') {
                 ui.showAlert('User not found', 'alert alert-danger');
+                
             } else {
-
                 ui.showProfile(data.profile);
                 ui.showRepos(data.repos);
             }
